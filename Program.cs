badfce1c68225f657c97d6e67f75f3e43e377415
unknown <kvkvachev@yandex.ru>
2022-11-29 10:22:40 +0300

    задача 38

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №38
+
+int n = 20;
+int [] mass  = new int[n];      
+Random rnd = new Random();      
+for (int i = 0; i < n; i++)
+    { mass[i] = rnd.Next(-10, 11);  
+    Console.Write($" {mass[i]} ");
+}
+int min = 0;
+int max = 0;
+foreach (int i in mass)  
+{
+    if (min > i) min = i;
+    if (max < i) max = i;
+}
+Console.WriteLine($" Cумма  максимального и минимального элемента равна: {max+min}");
